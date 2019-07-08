7c821cfed0be0d285dee380bc37d3e04240fe8dd
Aqudi <gjdigj@naver.com>
2019-07-08 22:52:46 +0900

    기본 설정

--- MemoryMapProject/settings.py
+++ MemoryMapProject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account.apps.AccountConfig',
 ]
 
 MIDDLEWARE = [
@@ -118,4 +119,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+
+# 1.python manage.py collectstatic
+# 2.Yes
+# 3.All static files are collected to STATIC_ROOT
+import os
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static'), ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
